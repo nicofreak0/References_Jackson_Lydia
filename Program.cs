95ab53e870ccecf961603ed4f5a516ee8673fe63
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-08-16 12:58:53 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace References_Jackson_Lydia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
